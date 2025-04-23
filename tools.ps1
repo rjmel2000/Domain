163313ec3bfd91dc310a2bbfc1ccced31c163716
rjmel2000 <rjmel2000ph@yahoo.com>
2025-04-23 13:03:46 -0500

    Update tools.ps1

--- tools.ps1
+++ tools.ps1
@@ -1,19 +1,23 @@
+          # Create tools directory
+          $ToolsDir = "C:\Tools"
+          New-Item -Path $ToolsDir -ItemType Directory -Force | Out-Null
+
           # Download PuTTY and Chrome
           [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
           $PuttyUrl = "https://the.earth.li/~sgtatham/putty/latest/w64/putty-64bit-0.83-installer.msi"
-          #$ChromeUrl = "https://dl.google.com/chrome/install/latest/chrome_installer.exe"
+          $ChromeUrl = "https://dl.google.com/chrome/install/latest/chrome_installer.exe"
           $PuttyInstaller = "$ToolsDir\putty.msi"
-          #$ChromeInstaller = "$ToolsDir\chrome_installer.exe"
+          $ChromeInstaller = "$ToolsDir\chrome_installer.exe"
           Invoke-WebRequest -Uri $PuttyUrl -OutFile $PuttyInstaller
-          #Invoke-WebRequest -Uri $ChromeUrl -OutFile $ChromeInstaller
+          Invoke-WebRequest -Uri $ChromeUrl -OutFile $ChromeInstaller
 
           # Install PuTTY and Chrome
           Start-Process "msiexec.exe" -ArgumentList "/i `"$PuttyInstaller`" /qn /norestart" -Wait
-          #Start-Process "$ChromeInstaller" -ArgumentList "/silent /install" -Wait
+          Start-Process "$ChromeInstaller" -ArgumentList "/silent /install" -Wait
           #Download SSMS
           Invoke-WebRequest -Uri "https://aka.ms/ssmsfullsetup" -OutFile "C:\Tools\SSMS-Setup.exe"
-          Start-Process -FilePath "C:\Tools\SSMS-Setup.exe" -ArgumentList "/install", "/quiet", "/norestart" -Wait
+          #Start-Process -FilePath "C:\Tools\SSMS-Setup.exe" -ArgumentList "/install", "/quiet", "/norestart" -Wait
 
           # Download Key
           $Url = "https://trendmicro-my.sharepoint.com/:u:/p/rommel_munoz/EUykZu2q8oxHmqkFLcODKCIBFruhCSj1bD8RmjU46DaDSw?download=1"
